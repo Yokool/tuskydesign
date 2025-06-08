22f695884a3e3b2a4a480e9820356ee1bd775d0a
Yokool <letsgamecze@gmail.com>
2025-06-08 18:26:35 +0200

    issue repro

--- packages/zoo/index.ts
+++ packages/zoo/index.ts
@@ -1,7 +1,13 @@
-import { getRandomAnimal } from "@tuskdesign/animals";
-import { getRandomName } from "@tuskdesign/names";
+// (0. only when testing in the repo => go to packages/animals, packages/names and packages/zoo and run npm run build in every folder) [also run npm install]
+// 1. Remove this import
+import { Animal } from "@tuskdesign/animals";
 
-const name = getRandomName();
-const animal = getRandomAnimal();
+// 2. Go to this line
+// 3. Put the cursor the the end of the word animal and trigger intellisense/autocompletion (by default ctrl+space)
+// 4. Auto imports don't offer the option to import from @tuskdesign/animals (expect it to offer the option when project references and aliases are setup correctly)
+const animal: Animal = {
+  name: "test",
+  sound: "test",
+};
 
-console.log(`${name} the ${animal.name} says ${animal.sound}!`);
+console.log(animal);
